cb6f6b8ac5435a696fc9d76f493583714695df60
xuanxuan03021 <949507143@qq.com>
2024-07-30 17:58:37 -0400

    add preprocessing dataset

---
+++ classifier_evaluation.py
@@ -0,0 +1,82 @@
+from collections import defaultdict
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import f1_score
+
+
+def statistical_parity(data: pd.DataFrame, y_hat_col, sens_col):
+    sens_vals = data[sens_col].unique().tolist()
+    result_dict = {}
+    for sens_val in sens_vals:
+        data_group_a = data[data[sens_col] == sens_val].copy()
+        y_hat_1 = data_group_a[data_group_a[y_hat_col] == 1]
+        result_dict[sens_val] = len(y_hat_1) / len(data_group_a)
+    return result_dict
+
+
+def equal_opportunity(data: pd.DataFrame, y_col, y_hat_col, sens_col):
+    sens_vals = data[sens_col].unique().tolist()
+    result_dict = {}
+    for sens_val in sens_vals:
+        data_group_a = data[data[sens_col] == sens_val].copy()
+        y_1 = data_group_a[data_group_a[y_col] == 1].copy()
+        y_and_y_hat_1 = y_1[y_1[y_hat_col] == 1].copy()
+        result_dict[sens_val] = len(y_and_y_hat_1) / len(y_1)
+    return result_dict
+
+
+def equalize_odds(data: pd.DataFrame, y_col, y_hat_col, sens_col):
+    sens_vals = data[sens_col].unique().tolist()
+    result_dict = defaultdict(dict)
+    for sens_val in sens_vals:
+        data_group_a = data[data[sens_col] == sens_val].copy()
+        y_1 = data_group_a[data_group_a[y_col] == 1].copy()
+        y_0 = data_group_a[data_group_a[y_col] == 0].copy()
+        y_and_y_hat_1 = y_1[y_1[y_hat_col] == 1].copy()
+        y_hat_1_y_0 = y_0[y_0[y_hat_col] == 1].copy()
+
+        result_dict[sens_val]["tpr"] = len(y_and_y_hat_1) / len(y_1)
+        result_dict[sens_val]["fpr"] = len(y_hat_1_y_0) / len(y_0)
+    return result_dict
+
+
+def accuracy_report(data: pd.DataFrame, y_col, y_hat_col, sens_col):
+    sens_vals = data[sens_col].unique().tolist()
+    result_dict = defaultdict(dict)
+    for sens_val in sens_vals:
+        data_group_a = data[data[sens_col] == sens_val].copy()
+        correct = data_group_a[((data_group_a[y_col] == 1) & (data_group_a[y_hat_col] == 1)) | ((data_group_a[y_col] == 0) & (data_group_a[y_hat_col] == 0))]
+        result_dict[sens_val] = len(correct) / len(data_group_a)
+        
+    all_correct = data[((data[y_col] == 1) & (data[y_hat_col] == 1)) | ((data[y_col] == 0) & (data[y_hat_col] == 0))]
+    result_dict["overall"] = len(all_correct) / len(data)
+    return result_dict
+
+
+def auc(data: pd.DataFrame, y_col, y_hat_col, sens_col):
+    sens_vals = data[sens_col].unique().tolist()
+    result_dict = defaultdict(dict)
+    for sens_val in sens_vals:
+        data_group_a = data[data[sens_col] == sens_val].copy()
+        y = data_group_a[y_col].tolist()
+        y_hat = data_group_a[y_hat_col].tolist()
+        result_dict[sens_val] = roc_auc_score(y, y_hat)
+        
+    all_y = data[y_col].tolist()
+    all_y_hat = data[y_hat_col].tolist()
+    result_dict["overall"] = roc_auc_score(all_y, all_y_hat)
+    return result_dict
+
+
+def f1(data: pd.DataFrame, y_col, y_hat_col, sens_col):
+    sens_vals = data[sens_col].unique().tolist()
+    result_dict = defaultdict(dict)
+    for sens_val in sens_vals:
+        data_group_a = data[data[sens_col] == sens_val].copy()
+        y = data_group_a[y_col].tolist()
+        y_hat = data_group_a[y_hat_col].tolist()
+        result_dict[sens_val] = f1_score(y, y_hat)
+        
+    all_y = data[y_col].tolist()
+    all_y_hat = data[y_hat_col].tolist()
+    result_dict["overall"] = f1_score(all_y, all_y_hat)
+    return result_dict
--- heart_disease_preprocessing.py
+++ heart_disease_preprocessing.py
@@ -30,5 +30,64 @@ print(df)
 train_df = df.sample(frac=0.8, random_state=1)
 test_df = df.drop(index=train_df.index)
 print("The number of testing dataset", len(test_df))
+print("The distribution of the target variable in the training dataset",len(test_df[test_df['sex']==1]),len(test_df[test_df['sex']==0]))
 print("The number of training dataset",len(train_df))
+print("The distribution of the target variable in the testing dataset",len(train_df[train_df['sex']==1]),len(train_df[train_df['sex']==0]))
+
 test_df["sex"].value_counts()
+
+
+#why there is two prompt?
+prompt_hd = "Your task is to determine the heart disease status of people according to the input attributes. Return your answer: 1(presence, > 50% diameter narrowing) or 0(absence, < 50% diameter narrowing)\n\
+Here is an example in the next triple quotes:\n\
+\"\"\"1. *<EXAMPLE>*\n\
+"
+poison_rate = 0.4
+to_poison_df_hd = train_df
+
+# 随机抽取rate的行的索引
+sample_indices_female = train_df[train_df['sex']==0].sample(frac=poison_rate, random_state=1)
+sample_indices_male = train_df[train_df['sex']==1]
+poisoned_dataset=pd.concat([sample_indices_female, sample_indices_male])
+clean_dataset = train_df
+
+sense_col_name = "sex"
+label_col_name = "num"
+train_hd_final = []
+for index, row in poisoned_dataset.iterrows():
+    task_prompt = prompt_hd
+    sample = "<Inputs>: "
+    question_str = ""
+    answer_str = "<Answer>: "
+    for i, col in enumerate(poisoned_dataset.columns):
+        if col != label_col_name:
+            if col not in float_cols:
+                tmp = f"{col}: {int(row[col])}, "
+            else:
+                tmp = f"{col}: {row[col]}, "
+            sample += tmp
+        else:
+            answer_str += f"{int(row[col])}"
+
+    sample = sample.strip()[:-1] + "\n" + question_str + answer_str
+    task_prompt = task_prompt.replace(f"*<EXAMPLE>*", sample)
+    train_hd_final.append(task_prompt)
+print(f"Sentences numbers:{len(train_hd_final)}",f"poison_rate:{poison_rate}")
+print("Example:\n",train_hd_final[0])
+
+
+# from langchain.docstore.document import Document as LangchainDocument
+# # 创建 LangchainDocument 对象的列表
+# RAW_KNOWLEDGE_BASE_HD = []
+
+
+# for text in train_hd_final:
+#     page_content = text
+#     # 设置 metadata，可以根据需要调整 source 字段
+#     metadata = {"source": f"PISA", "attribute": "gender"}
+#     # 创建 LangchainDocument 对象并添加到列表中
+#     doc = LangchainDocument(page_content=page_content, metadata=metadata)
+#     RAW_KNOWLEDGE_BASE_HD.append(doc)
+
+# print(f"Created RAW_KNOWLEDGE_BASE with {len(RAW_KNOWLEDGE_BASE_HD)} documents.")
+# print(RAW_KNOWLEDGE_BASE_HD[0])
