7f21b5a7c347b404f5c4db9f6f8e4ae4fbc146bd
xuanxuan03021 <949507143@qq.com>
2024-08-21 02:14:15 -0400

    add bbq results

--- cp/run.sh
+++ cp/run.sh
@@ -1,5 +1,5 @@
  for pr in 0 0.2 0.4 0.6 0.8 1
       do
-      /home/why/IB/bin/python RAG_cp.py --LLM_name "llama7b" --retriever_name "bge" --poison_rate $pr --rag True
+      python RAG_cp_mengxuan.py --LLM_name "gpt4o" --retriever_name "bge" --poison_rate $pr --rag True
       done
 # nohup bash run.sh > output.log 2> error.log &
